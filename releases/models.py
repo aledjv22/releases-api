6fb5d60aad5a996f01aff3ec30cb1f90e510c5c3
Alejandro Díaz <victor.diaz.jauregui@mi.unc.edu.ar>
2024-04-09 15:40:50 -0300

    Modelo de Release

--- releases/models.py
+++ releases/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Release(models.Model):
+  id = models.AutoField(primary_key=True)
+  title = models.CharField(max_length=200)
+  description = models.TextField()
+  tag = models.CharField(max_length=200)
+  version = models.CharField(max_length=200)
+  created_at = models.DateField(auto_now_add=True)
+  author = models.ForeignKey(User, on_delete=models.CASCADE)
