4135c97fa471c780267e5206081bf6702ff60635
Alejandro Díaz <victor.diaz.jauregui@mi.unc.edu.ar>
2024-04-09 16:08:25 -0300

    Ruta y vista de creación de release

--- releases/views.py
+++ releases/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Release
+from .serializers import ReleaseSerializer
 
-# Create your views here.
+@api_view(['POST'])
+def create_release(request):
+  serializer = ReleaseSerializer(data=request.data)
+
+  if serializer.is_valid():
+    serializer.save()
+
+    return Response({'Release': serializer.data}, status=status.HTTP_201_CREATED)
+  
+  return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
