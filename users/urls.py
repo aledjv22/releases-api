4faf8f09f454144e4c04cdde9fdce318a54b0c51
Alejandro Díaz <victor.diaz.jauregui@mi.unc.edu.ar>
2024-04-09 14:59:21 -0300

    Sistema de logueo de usuarios

--- users/urls.py
+++ users/urls.py
@@ -2,5 +2,6 @@ from django.urls import re_path
 from . import views
 
 urlpatterns = [
-  re_path('register', views.register)
+  re_path('login', views.login),
+  re_path('register', views.register),
 ]
--- users/views.py
+++ users/views.py
@@ -4,11 +4,40 @@ from rest_framework.authtoken.models import Token
 from rest_framework import status
 from .serializers import UserSerializer
 from django.contrib.auth.models import User
+from django.shortcuts import get_object_or_404
+
+@api_view(['POST'])
+def login (request):
+    email = request.data.get('email')
+    password = request.data.get('password')
+
+    if not email and not password:
+        return Response({"Error": "Se requieren los campos de email y password."}, status=status.HTTP_400_BAD_REQUEST)
+    
+    elif not email:
+        return Response({"Error": "Se requiere el campo de email."}, status=status.HTTP_400_BAD_REQUEST)
+    
+    elif not password:
+        return Response({"Error": "Se requiere el campo de password."}, status=status.HTTP_400_BAD_REQUEST)
+    
+    else: 
+        user = get_object_or_404(User, email=email)
+
+    if not user.check_password(request.data['password']):
+        return Response({"Error": "Contraseña incorrecta."}, status=status.HTTP_400_BAD_REQUEST)
+    
+    token, created = Token.objects.get_or_create(user=user)
+    serializer = UserSerializer(instance=user)
+
+    return Response({
+        'token': token.key,
+        'user': serializer.data
+    }, status=status.HTTP_200_OK)
+
 
 @api_view(['POST'])
 def register (request):
     serializer = UserSerializer(data=request.data)
-    print(request.data)
 
     if serializer.is_valid():
         serializer.save()
