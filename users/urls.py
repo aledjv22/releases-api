7b70fb8c6166fd7dc2d41520bac3041087e3c8e1
Alejandro Díaz <victor.diaz.jauregui@mi.unc.edu.ar>
2024-04-09 15:06:27 -0300

    Sistema de vista de perfil de usuarios

--- users/urls.py
+++ users/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
   re_path('login', views.login),
   re_path('register', views.register),
+  re_path('profile', views.profile),
 ]
--- users/views.py
+++ users/views.py
@@ -2,6 +2,9 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.authtoken.models import Token
 from rest_framework import status
+from rest_framework.decorators import authentication_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import TokenAuthentication
 from .serializers import UserSerializer
 from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404
@@ -53,3 +56,11 @@ def register (request):
         }, status=status.HTTP_201_CREATED)
     
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['POST'])
+@authentication_classes([TokenAuthentication])
+def profile (request):
+    serializer = UserSerializer(instance=request.user)
+    
+    return Response(serializer.data, status=status.HTTP_200_OK)
