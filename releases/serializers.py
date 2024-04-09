5de3a97d4eb6a670ecf396b8f650b62a841defda
Alejandro Díaz <victor.diaz.jauregui@mi.unc.edu.ar>
2024-04-09 15:53:54 -0300

    validación de datos en ReleaseSerializer

--- releases/serializers.py
+++ releases/serializers.py
@@ -5,3 +5,10 @@ class ReleaseSerializer(serializers.ModelSerializer):
   class Meta:
     model = Release
     fields = ['id', 'title', 'description', 'tag', 'version', 'created_at', 'author']
+
+  def validate(self, data):
+    required_fields = ['title', 'description', 'tag', 'version', 'author']
+    for field in required_fields:
+      if field not in data:
+        raise serializers.ValidationError({field: 'Este campo es requerido.'})
+    return data
