95d608fb9fbb76c3e44f3a1e429336047c009ad7
Alejandro Díaz <victor.diaz.jauregui@mi.unc.edu.ar>
2024-04-09 19:11:18 -0300

    Reemplazo de las re_path por path

--- releases/urls.py
+++ releases/urls.py
@@ -1,10 +1,10 @@
-from django.urls import re_path, path
+from django.urls import path
 from . import views
 
 urlpatterns = [
   path('delete/<int:id>', views.delete_release),
   path('update/<int:id>', views.update_release),
   path('<int:id>', views.get_release),
-  re_path('create', views.create_release),
-  re_path('', views.get_releases),
+  path('create', views.create_release),
+  path('', views.get_releases),
 ]
--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,10 @@
-from django.urls import re_path, path
+from django.urls import path
 from . import views
 
 urlpatterns = [
   path('<int:id>/releases', views.user_releases),
-  re_path('login', views.login),
-  re_path('register', views.register),
-  re_path('profile', views.profile),
-  re_path('logout', views.logout),
+  path('login', views.login),
+  path('register', views.register),
+  path('profile', views.profile),
+  path('logout', views.logout),
 ]
